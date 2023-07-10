48fdbd7685cc4d38b87b15b4d002fc2719c54d1f
SofiGuchkova <123586646+SofiGuchkova@users.noreply.github.com>
2023-07-10 13:36:04 +0200

    Java Basic (cоздание переменных String и Int)
    установила JDK 11, 20
    создала два проекта Java, переключить в настройках один на 11, второй на 20
    В методе main создала несколько переменных типа String и int, вывела их через "System.out.println" пользователю
    
    В этом же методе добавила еще 2 переменных int, произвела сложение, вычитание, деление и умножение, вывела их через "System.out.println" пользователю

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 2
+        String name = "Alex";
+        int age = 29;
+        String city = "Novi Sad, Serbia";
+        int weight = 90;
+
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Город: " + city);
+        System.out.println("Вес: " + weight);
+
+        // Задание 3
+    int noname1 = 10;
+    int noname2 = 5;
+    int sum = noname1 + noname2;
+            int sub = noname1 - noname2;
+            int div = noname1 / noname2;
+            int mult = noname1 * noname2;
+
+        System.out.println("1 число: " + noname1);
+        System.out.println("2 число: " + noname2);
+        System.out.println("Сложение: " + sum);
+        System.out.println("Вычитание: " + sub);
+        System.out.println("Деление: " + div);
+        System.out.println("Умножение: " + mult);}
+}
