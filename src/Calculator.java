7c0fdd256ad6268264dcfe193d2e22c0ad00a4d3
Just Sophie <123586646+SofiGuchkova@users.noreply.github.com>
2023-08-07 19:05:39 +0200

    HW_Java_02_08

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+
+    int add(int val, int val1) {
+        return val + val1;
+
+    }
+
+
+     int subtract(int val, int val1) {
+        return val - val1;
+
+     }
+
+    public int multi(int val, int val1) {
+        return val * val1;
+    }
+    int divide(int val, int val1) {
+        return val / val1;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        WorldState worldState = new WorldState(); NumChanges numChanges = new NumChanges();
-        Scanner scanner = new Scanner(System.in);
-
-//Вариант 1 = поменять местами значения
-        System.out.print("Введите значение первого и второго чисел: ");
-        numChanges.printOutNum(scanner.nextInt(), scanner.nextInt());
-
-        //Вариант 2 = поменять местами значения
-        numChanges.changeNum();
-
-
-
-        // Задание 2 - про состояние мира, вызываем методы.
-        worldState.isWorldNegativelyGood();
-worldState.isWorldPositivelyGood();
-
-
+new MathQuiz().start();
     }
 }
